b09c51c80b00e7751ceb01186cfd1cb4fc4da493
Kevin Raoofi <kraoofi@itcurves.net>
2017-05-13 18:06:29 -0400

    Added gitignore

--- cron/ore_miner.php
+++ cron/ore_miner.php
@@ -1,13 +1,23 @@
-<?php 
-
-$url = "https://go.ourrevolution.com/page/event/search_results?orderby=day&state=MD&country=US&format=json";
-#$message = "This is my IP address".$_SERVER['REMOTE_ADDR'];
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, $url);
-$result = curl_exec($ch);
-curl_close($ch);
-    
-file_put_contents("ore_data.json", $result);   
-
+<?php 
+
+
+
+$url = "https://go.ourrevolution.com/page/event/search_results?orderby=day&state=MD&country=US&format=json";
+
+#$message = "This is my IP address".$_SERVER['REMOTE_ADDR'];
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+curl_setopt($ch, CURLOPT_URL, $url);
+
+$result = curl_exec($ch);
+
+curl_close($ch);
+
+
+
+file_put_contents("ore_data.json", $result);
--- js/ore.js
+++ js/ore.js
@@ -1,40 +1,79 @@
-jQuery(document).ready(
-/*    
-    // Using the core $.ajax() method
-jQuery.ajax({
- 
-    // The URL for the request
-    url: "https://go.ourrevolution.com/page/event/search_results",
- 
-    // The data to send (will be converted to a query string)
-    data: {
-        orderby: 'day',
-        state: 'MD',
-        country: 'US',
-        format: 'json'
-    },
- 
-    // Whether this is a POST or GET request
-    type: "GET",
- 
-    // The type of data we expect back
-    dataType : "json",
-})
-  // Code to run if the request succeeds (is done);
-  // The response is passed to the function
-  .done(function( data ) {
-     alert(data.settings.count);
-  })
-  // Code to run if the request fails; the raw request and
-  // status codes are passed to the function
-  .fail(function( xhr, status, errorThrown ) {
-    alert( "Sorry, there was a problem!" );
-    console.log( "Error: " + errorThrown );
-    console.log( "Status: " + status );
-    console.dir( xhr );
-  })
-  // Code to run regardless of success or failure;
-  .always()
-*/  
-    
+jQuery(document).ready(
+
+/*
+
+    // Using the core $.ajax() method
+
+jQuery.ajax({
+
+
+
+    // The URL for the request
+
+    url: "https://go.ourrevolution.com/page/event/search_results",
+
+
+
+    // The data to send (will be converted to a query string)
+
+    data: {
+
+        orderby: 'day',
+
+        state: 'MD',
+
+        country: 'US',
+
+        format: 'json'
+
+    },
+
+
+
+    // Whether this is a POST or GET request
+
+    type: "GET",
+
+
+
+    // The type of data we expect back
+
+    dataType : "json",
+
+})
+
+  // Code to run if the request succeeds (is done);
+
+  // The response is passed to the function
+
+  .done(function( data ) {
+
+     alert(data.settings.count);
+
+  })
+
+  // Code to run if the request fails; the raw request and
+
+  // status codes are passed to the function
+
+  .fail(function( xhr, status, errorThrown ) {
+
+    alert( "Sorry, there was a problem!" );
+
+    console.log( "Error: " + errorThrown );
+
+    console.log( "Status: " + status );
+
+    console.dir( xhr );
+
+  })
+
+  // Code to run regardless of success or failure;
+
+  .always()
+
+*/
+
+
+
 );
